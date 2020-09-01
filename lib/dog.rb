7fe07aae10b1a0ef65628332428fdbdad2a70aa1
Taylor Sieling <taylorasieling@gmail.com>
2020-09-01 00:57:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog 
   
-  def name=(dog_name)
-    @name = dog_name
+  def name=(name)
+    @name = name
   end
   
   def name
-    @name = dog_name
+    @name = name
   end
   
   def breed=(dog_breed)
