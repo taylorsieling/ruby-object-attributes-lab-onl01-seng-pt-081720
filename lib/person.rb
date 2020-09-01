8a58a418017d3a44477f56c0e8bc17bb8e4ac0d4
Taylor Sieling <taylorasieling@gmail.com>
2020-09-01 01:00:53 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
 class Person
   
   def name=(person_name)
-    @person_name = name
+    @person_name = person_name
   end
   
   def name
-    @person_name = name
+    @person_name = person_name
   end
   
 end
